81d6ddac6922869bdbf828aa1b059b7bb5a9a5f9
ga021396 <ga021396@gmail.com>
2019-02-27 10:24:04 +0800

    init App

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+     <div id="nav">
+        <h1 id="hav">HaveFun</h1>
+        <font-awesome-icon icon="coffee" class="icon"/>
+        <input type="text" placeholder="Explore your own activites">
+    </div>
+    <div id="search">
+        <div id="location">
+             <h2>Location</h2>
+             <form>
+                 <select class="YourLocation">
+                 <option value="0">ALL</option>
+                 <option value="1">中正</option>
+                 <option value="2">大同</option>
+                 <option value="3">中山</option>
+                 <option value="4">松山</option>
+                 <option value="5">大安</option>
+                 <option value="6">萬華</option>
+                 <option value="7">信義</option>
+                 <option value="8">士林</option>
+                 <option value="9">內湖</option>
+                 <option value="10">南港</option>
+                 </select>
+            </form>
+        </div>
+        <div id="date">
+             <h2>Date</h2>
+            <div class="date">
+                <span>from</span>
+                <input id="date" type="date" value="2017-06-01">
+            </div>
+            <div class="date">
+                <span style="margin-left: 18px;">to</span>
+                <input id="date" type="date" value="2017-06-01">
+            </div>
+        </div>
+        <div id="categories">
+            <h2>Categories</h2>
+            <ul>
+                <li><input type="checkbox" id="all">All</li>
+                <li><input type="checkbox">Entertainment</li>
+                <li><input type="checkbox">Food</li>
+                <li><input type="checkbox">Learing</li>
+                <li><input type="checkbox">Outdoors</li>
+            </ul>
+        </div>
+    </div>
+    <div id="body">
+        <h2 class="howmany">Showing 15 results by...</h2>
+        <div class="result">
+            <span class="locaTag">taipei<i class="far fa-times-circle"></i></span>
+            <span>Sports<i class="far fa-times-circle"></i></span>
+        </div>
+        <card></card>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/Card.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
@@ -23,6 +75,103 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body,html{
+    background-color: #F2F2F2;
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+}
+#nav{
+    width:100%;
+    height:92px;
+    background-color: #7828B4;
+    display: flex;
+    align-items: center;
+    
+    
+}
+#nav h1{
+    color: white;
+    margin-left: 7%;
+}
+#nav input{
+    width: 388px;
+    height: 37px;
+    padding-left: 10px;
+    margin-left: 10px;
+    background-color: #7828B4;
+    color: white;
+    border: none;
+    border-bottom:1px solid rgba(255,255,255,0.5);
+}
+.icon{
+     color: white;
+     margin-left: 12%;
+}
+::placeholder { /* Chrome, Firefox, Opera, Safari 10.1+ */
+    color:rgba(255,255,255,0.5);
+}
+/*search*/
+#search{
+    float: left;
+    width: 25%;
+    height: auto;
+    margin-left: 3%;
+    background-color: #EBEBEB ;
+    max-width: 300px;
+}
+#search div{
+    padding: 0 12px;
+}
+#location select{
+    width:80%;
+    height: 30px;
+    margin-bottom: 20px;
+}
+#location,#date{
+    border-bottom: 1px solid rgba(0,0,0,0.2);
+}
+#date .date{
+    margin-left: -20px;
+}
+#date input{
+    border: none;
+    width: 58%;
+    margin-bottom: 20px;
+    margin-right: 10%;
+    margin-left: 5%;
+    height: 30px;
+}
+#categories ul{
+    text-align: left;
+}
+#categories ul li{
+    list-style: none;
+}
+
+#body{
+    float: left;
+    width:65%;
+    max-width: 780px;
+    height:auto;
+    padding-left: 5%;
+}
+#body h2{
+    font-weight: 400;
+    text-align: left;
+}
+.result{
+  display: flex;
+}
+.result span{
+    border: 1px solid #9013FE;;
+    padding:5px 10px;
+    border-radius: 15px;
+    color: #9013FE;
+    margin-right: 8px;
+}
+.result i{
+    padding-left: 5px;
+    color: #9013FE;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,78 @@
+<template>
+        <div>
+         <div class="card" v-for="(item,index) in list" :key="index">
+            <div class="img">
+                 <img src="Images/001.jpg">
+            </div>
+            <div class="introduction">
+                <h2 class="name">hello world</h2>
+                <p class="purpose">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...</p>
+                <span class="aut">Ethan Foster</span>
+                <span class="cardResult">Entertainment</span>
+                <div class="loc">
+                    <span ><font-awesome-icon icon="coffee" /><span class="address">Taipei city</span></span>
+                    <span ><font-awesome-icon icon="coffee" /><span class="opentime">2018/06/07-2018/06/17</span></span>
+                </div>
+            </div>
+        </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'card',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      list:[1,2,3,4,5]
+    }
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.card{
+    margin: 25px 0 ;
+    width: 100%;
+    display: flex;
+    height: 220px;
+}
+.card img{
+    width: 220px;
+    height: 100%;
+}
+.card .introduction{
+    text-align: left;
+    padding-left: 20px;
+    width:100%;
+    background-color:white;
+}
+.introduction h2{
+     color: #9013FE;
+}
+.aut{
+    font-weight: 600;
+    margin-right: 10px;
+}
+.cardResult{
+    color: white;
+    background-color: #D7D7D7; 
+    padding:2px 15px;
+    border-radius: 15px;    
+    font-style: italic;
+}
+.loc{
+    color: #9B9B9B;
+    margin-top: 15px;
+}
+.loc i{
+    color:black;
+    margin-right: 8px;
+}
+.loc span{
+    margin-right: 20px;
+}
+</style>
