46e78fa6351b114ac508313da956019496e167d4
ga021396 <ga021396@gmail.com>
2019-03-17 16:16:33 +0800

    add firebase

---
+++ src/assets/firebase.js
@@ -0,0 +1,34 @@
+// src/db.js
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAOsja00FEM9p1HR0xvnSe0aMwuVm_D7TE",
+    authDomain: "animefilter.firebaseapp.com",
+    databaseURL: "https://animefilter.firebaseio.com",
+    projectId: "animefilter",
+    storageBucket: "animefilter.appspot.com",
+    messagingSenderId: "527533106109"
+  };
+  firebase.initializeApp(config);
+  const firestore = firebase.firestore();
+  const doc =firestore.doc("users/user01")
+  doc.set({
+          name: "cliff",
+          gender: "male",
+          born: 1996,
+          cake:'good'
+      })
+      .then(function (docRef) {
+          console.log("Document written with ID: ", docRef);
+      })
+      .catch(function (error) {
+          console.error("Error adding document: ", error);
+      });
+  doc.get().then((doc)=>console.log(doc))
+    
+
+export const db = firebase.firestore()
